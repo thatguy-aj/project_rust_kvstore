40096937bb4872d10f9ae19d20972c0a9e0c803c
thatguy-aj <andrewcj26@gmail.com>
2026-07-03 17:10:20 -0400

    Renames hello.rs to main.rs and updates print statements

--- src/hello.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    println!("Hello, world! This is a Test File to chekc if Git is working correctly");
-}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("Hello, world! This is a Test File to chekc if Git is working correctly!");
+    println!("Goodbye for Now!");
+}
